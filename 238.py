089af2a2257674611698f78d19deb588495fd6c5
Thej Venkat P <thej.venkat@gmail.com>
2024-03-15 18:42:38 +0530

    DQ 15/3/24 238

--- 238.py
+++ 238.py
@@ -14,3 +14,26 @@ class Solution:
             ans[i] *= curr
             curr *= nums[i]
         return ans
+    
+    
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre_prod = 1
+        suf_prod = 1
+        num_len = len(nums)
+        pre_l = [1] * num_len
+        suf_l = [1] * num_len
+        res = [1] * num_len
+
+        for i in range(num_len):
+            pre_l[i] = pre_prod
+            pre_prod *= nums[i]
+
+        for j in range(num_len-1, -1, -1):
+            suf_l[j] = suf_prod
+            suf_prod *= nums[j]
+
+        for i in range(num_len):
+            res[i] = pre_l[i] * suf_l[i]
+
+        return res
