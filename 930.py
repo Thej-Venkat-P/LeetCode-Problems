45d28039ea2406f7f7989537c506c12448383fd3
Thej Venkat P <thej.venkat@gmail.com>
2024-03-15 19:02:58 +0530

    930 - Another Solution

--- 930.py
+++ 930.py
@@ -17,3 +17,23 @@ class Solution:
                 ans += r - l + 1
             return ans 
         return max_occ(goal) - max_occ(goal - 1)
+
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        i = 0
+        ans = 0
+        count = 0
+        for j in range(len(nums)):
+            if nums[j] == 1:
+                goal -= 1
+                count = 0
+            while goal == 0 and i <= j:
+                goal += nums[i]
+                count += 1
+                i += 1
+            while goal < 0:
+                goal += nums[i]
+                i += 1
+            ans += count
+        return ans
