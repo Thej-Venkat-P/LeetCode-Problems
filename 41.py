8a8bbd38327a212e6fc55f27213e05265c80ac1a
Thej Venkat P <thej.venkat@gmail.com>
2024-03-26 18:21:28 +0530

    faster execution

--- 41.py
+++ 41.py
@@ -1,35 +1,37 @@
 # url: https://leetcode.com/problems/first-missing-positive/
 
 
-# Nums[i] contains both the number of occurences of i as n//i and the value at nums[i] as n%i.
 class Solution:
+    # Nums[i] contains both the number of occurences of i as n//i and the value at nums[i] as n%i.
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums.append(0)
-        n = len(nums)
-        for i in range(n):
-            if nums[i] not in range(1,n):
+        nums.append(0) # To handle the case where n is present in nums, we need to have n+1 elements in nums.
+        l = len(nums)
+
+        for i, n in enumerate(nums):
+            if n not in range(1, l):
                 nums[i] = 0
-        print(nums)
-        for i in range(n):
-            index = nums[i] % n
-            nums[index] += n
-        for i in range(1,n):
-            if nums[i] < n:
-                return i
-        return n
 
+        for i, n in enumerate(nums):
+            if n%l != 0:
+                nums[n % l] += l
 
-# Cycle sort
+        for i,n in enumerate(nums[1:]):
+            if n < l:
+                return i + 1
 
-class Solution:
+        return l
+
+    # Swapping till we get correct index for each element.
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
-        for i in range(len(nums)):
-            val = nums[i]
-            while val > 0 and val <= n and val != i + 1 and val != nums[val - 1]:
-                nums[i],nums[val - 1] = nums[val - 1],nums[i]
-                val = nums[i]
-        for i,num in enumerate(nums):
-            if num != i+1:
+        
+        for i, num in enumerate(nums):
+            while 1<=num<=n and num != nums[num - 1]:
+                nums[i], nums[num - 1] = nums[num - 1], nums[i]
+                num = nums[i]
+
+        for i in range(n):
+            if nums[i] != i+1:
                 return i+1
+
         return n + 1
