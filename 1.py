40ef7c13490d461fe02e9d8dfe36f73aabb592a9
Thej Venkat P <thej.venkat@gmail.com>
2024-03-29 22:32:31 +0530

    Remove duplicate class definition and extra blank lines

--- 1.py
+++ 1.py
@@ -10,8 +10,6 @@ class Solution:
             d[e] = i
         return []
 
-
-class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i in range(len(nums)):
