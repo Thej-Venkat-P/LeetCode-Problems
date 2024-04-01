315e8acd6f703f572547e95ca4521dc64a89e697
Thej Venkat P <thej.venkat@gmail.com>
2024-04-01 13:08:22 +0530

    DQ 01/04/23 58

---
+++ 58.py
@@ -0,0 +1,13 @@
+# url: https://leetcode.com/problems/length-of-last-word/
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+        ans = 0
+        while i >= 0 and s[i] == " ":
+            i -= 1
+        while i >= 0 and s[i] != " ":
+            ans += 1
+            i -= 1
+        return ans
