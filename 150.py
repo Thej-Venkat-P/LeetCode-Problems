22708ef67daf55246d7241de092e764c41cd1155
Thej Venkat P <thej.venkat@gmail.com>
2024-04-27 14:48:48 +0530

    added solution

--- 150.py
+++ 150.py
@@ -16,3 +16,25 @@ class Solution:
             else:
                 stack.append(int(token))
         return stack[0]
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token in ("+", "-", "*", "/"):
+                val2 = stack.pop()
+                val1 = stack.pop()
+                if token == "+":
+                    stack.append(val1 + val2)
+                elif token == "-":
+                    stack.append(val1 - val2)
+                elif token == "*":
+                    stack.append(val1 * val2)
+                elif token == "/":
+                    stack.append(int(val1 / val2))
+                print(stack[-1])
+            else:
+                stack.append(int(token))
+
+        return stack[0]
+            
