e70ef6bb233c9a1742cf47e31dfc5e4003cfe7f4
Thej Venkat P <thej.venkat@gmail.com>
2024-05-25 01:39:16 +0530

    2597 Linear Solution

--- 2597.py
+++ 2597.py
@@ -17,3 +17,38 @@ class Solution:
         mp = defaultdict(int)
         ans = self.dfs(nums, 0, k, mp)
         return ans - 1
+
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        count = Counter(nums)
+        found = set()
+        groups = []
+        
+        for num in count:
+            if num in found:
+                continue
+            groups.append([])
+            temp = num
+            while temp in count:
+                temp -= k
+            temp += k
+            while temp in count:
+                found.add(temp)
+                groups[-1].append(temp)
+                temp += k
+
+        @lru_cache(None)
+        def find_bs(group, i):
+            if i >= len(group):
+                return 1
+            curr = group[i]
+            taken = ((2 ** count[curr]) - 1) * find_bs(group, i+2)
+            not_taken = find_bs(group, i+1)
+            return taken + not_taken
+
+        res = 1
+        for group in groups:
+            bs = find_bs(tuple(group), 0)
+            res *= bs
+
+        return res - 1
