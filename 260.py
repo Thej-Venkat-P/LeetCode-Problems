2e212a13cb287225da3c5bfcfd26fca6ae94f3e1
Thej Venkat P <thej.venkat@gmail.com>
2024-06-01 09:27:52 +0530

    DQ 31/05/2024 260

--- 260.py
+++ 260.py
@@ -15,3 +15,26 @@ class Solution:
                 second ^= num
 
         return [first, second]
+    
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor_sum = 0
+        for num in nums:
+            xor_sum ^= num
+        
+        tmp = xor_sum
+        first_change = 0
+        while tmp & 1 == 0:
+            tmp = tmp >> 1
+            first_change += 1
+        
+        p = 1 << first_change
+        num1 = num2 = 0
+        for num in nums:
+            if num & p:
+                num1 ^= num
+            else:
+                num2 ^= num
+        return [num1, num2]
+            
